a241fdb4ae3cf98d260f0123f7f1d79b738df759
William West <wwest466@gmail.com>
2020-09-17 15:42:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complextion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   will_attributes = {
     :name => "Will",
     :birthday => "03/22/200",
