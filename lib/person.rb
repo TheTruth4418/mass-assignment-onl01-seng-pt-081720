c8bed0d9a1c6b33c143597101e409af073673705
William West <wwest466@gmail.com>
2020-09-17 15:39:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,20 @@
 class Person
   attr_accessor :name
-  will_attributes = { :name => "Will" }
+  will_attributes = { 
+    :name => "Will",
+    :birthday => "03/22/200",
+    :hair_color => "black",
+    :eye_color => "brown",
+    :height => "tall",
+    :weight => "good",
+    :handed => "right",
+    :complexion => "decent",
+    :t_shirt_size => "medium",
+    :wrist_size => "small",
+    :glove_size => "normal",
+    :pant_length => "32",
+    :pant_width => "32"
+   }
   def initialize(attributes)
     attributes.each{|x,y| self.send(("#{x}="), y)}
   end
