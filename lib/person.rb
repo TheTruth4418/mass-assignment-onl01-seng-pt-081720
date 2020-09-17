574763e13b6e912968891e6e8fde2789d15cfcf4
William West <wwest466@gmail.com>
2020-09-17 15:17:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor :name
   will_attributes = { :name => "Will" }
   def initialize(attributes)
-    attributes.each{|x,y| self.send(("#{x}"), y)}
+    attributes.each{|x,y| self.send(("#{x}="), y)}
   end
 end
